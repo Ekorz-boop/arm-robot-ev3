f5fe2f3cc4b4b38de430a9635a98bd6dfad4564b
Simon Lindqvist <muckot@gmail.com>
2023-04-05 15:28:58 +0200

    barebone pick-up functionality

--- main.py
+++ main.py
@@ -21,28 +21,25 @@ horizontal_axis = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12, 36])
 vertical_axis.control.limits(speed=60, acceleration=120)
 horizontal_axis.control.limits(speed=60, acceleration=120)
 
+
+claw.run_until_stalled(200, then=Stop.COAST, duty_limit=50)
+claw.reset_angle(0)
+claw.run_target(200, -90)
+vertical_axis.run_until_stalled(-20, then=Stop.COAST, duty_limit=50)
+vertical_axis.reset_angle(0)
+
+
+
+
 # Write your program here.
 def pick_up():
     """Function that makes the claw grip and move upward (picking up)"""
-    for i in range(1, 10):
-        claw.run(10)
-        
-
-
-    for i in range(1, 10):
-        vertical_axis.run(10)
-        vertical_axis.reset_angle(0)
+    claw.run_until_stalled(-100, then=Stop.COAST, duty_limit=50)
+    vertical_axis.run_target(20, 86, then=Stop.HOLD)
 
 
 def main():
     pick_up()
-
-
-def stay_up():
-    on = True 
-    while on == True:
-    {
-
-    }
+    
 if __name__ == "__main__":
     main()
